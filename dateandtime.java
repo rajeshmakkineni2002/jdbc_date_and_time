1904e1608de6a81a74b8d6282c7580bb596fb72a
Rajesh-makkineni <makkinenirajesh@gmail.com>
2025-08-17 23:30:01 +0100

    Add Java files

---
+++ dateandtime.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+public class Main 
+{
+    public static void main(String args [])
+    {
+        Date udate =new Date();
+        System.out.println("utilDate is : :" + udate);
+
+        long value =udate.getTime();
+
+        java.sql.Date sqlDate = new java.sql.Date(value);
+        System.out.println("sqldate is : : " + sqlDate);
+    }
+}
